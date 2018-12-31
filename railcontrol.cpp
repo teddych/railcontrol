82583b2bf50fb4b4ee7a3e8c3dd488c3bd15b86c
Teddy <teddy@teddy.ch>
2018-12-31 09:36:09 +0100

    Simplify code

--- railcontrol.cpp
+++ railcontrol.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>		//printf
 #include <cstdlib>		//exit(0);
 #include <cstring>		//memset
-#include <dlfcn.h>              // dlclose
+#include <dlfcn.h>		// dlclose
 #include <iostream>
 #include <signal.h>
 #include <sstream>
@@ -20,37 +20,33 @@ using std::string;
 
 static volatile unsigned int runRailcontrol;
 
-void stopRailControlSignal(int signo) {
+void stopRailControlSignal(int signo)
+{
 	xlog("Stopping railcontrol requested by signal %i", signo);
 	runRailcontrol = false;
 }
 
-void stopRailControlWebserver() {
+void stopRailControlWebserver()
+{
 	xlog("Stopping railcontrol requested by a webclient");
 	runRailcontrol = false;
 }
 
-void stopRailControlConsole() {
+void stopRailControlConsole()
+{
 	xlog("Stopping railcontrol requested by a console client");
 	runRailcontrol = false;
 }
 
-int main (int argc, char* argv[]) {
+int main (int argc, char* argv[])
+{
 	signal(SIGINT, stopRailControlSignal);
 	signal(SIGTERM, stopRailControlSignal);
 
 	runRailcontrol = true;
 	xlog("Starting railcontrol");
 
-	string configfile;
-	if (argc == 2) {
-		configfile.append(argv[1]);
-	}
-	else {
-		configfile.append("railcontrol.conf");
-	}
-
-	Config config(configfile);
+	Config config(argc == 2 ? argv[1] : "railcontrol.conf");
 
 	// init manager that does all the stuff in a seperate thread
 	Manager m(config);
@@ -61,7 +57,8 @@ int main (int argc, char* argv[]) {
 	struct timeval tv;
 	fd_set set;
 
-	do {
+	do
+	{
 		tv.tv_sec = 1;
 		tv.tv_usec = 0;
 		// Zero out the fd_set - make sure it's pristine
@@ -77,7 +74,8 @@ int main (int argc, char* argv[]) {
 		int ret = TEMP_FAILURE_RETRY(select(FD_SETSIZE, &set, NULL, NULL, &tv));
 
 		// only read STDIN if there really is something to read
-		if (ret > 0 && FD_ISSET(STDIN_FILENO, &set)) {
+		if (ret > 0 && FD_ISSET(STDIN_FILENO, &set))
+		{
 			read(STDIN_FILENO, &input, sizeof(input));
 		}
 	} while (input != 'q' && runRailcontrol);
