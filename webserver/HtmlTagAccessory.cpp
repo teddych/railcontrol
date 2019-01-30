07593bff0d6a6289b09e7a45c969cdfe256e92cf
Teddy <teddy@teddy.ch>
2019-01-30 17:54:50 +0100

    Simplify HtmlTagAccessory

--- webserver/HtmlTagAccessory.cpp
+++ webserver/HtmlTagAccessory.cpp
@@ -10,30 +10,13 @@ namespace webserver
 {
 	HtmlTagAccessory::HtmlTagAccessory(const datamodel::Accessory* accessory)
 	{
-		layoutPosition_t posX;
-		layoutPosition_t posY;
-		layoutPosition_t posZ;
-		layoutItemSize_t w;
-		layoutItemSize_t h;
-		layoutRotation_t r;
-		accessory->position(posX, posY, posZ, w, h, r);
 		accessoryState_t state = accessory->GetState();
-		Init(accessory->objectID, accessory->name, posX, posY, posZ, state, accessory->address);
-	}
 
-	void HtmlTagAccessory::Init(const accessoryID_t accessoryID,
-		const std::string& name,
-		const layoutPosition_t posX,
-		const layoutPosition_t posY,
-		const layoutPosition_t posZ,
-		const accessoryState_t state,
-		const address_t address)
-	{
-		unsigned int layoutPosX = posX * 35;
-		unsigned int layoutPosY = posY * 35;
+		unsigned int layoutPosX = accessory->posX * 35;
+		unsigned int layoutPosY = accessory->posY * 35;
 
 		HtmlTag div1("div");
-		string accessoryIdString = to_string(accessoryID);
+		string accessoryIdString = to_string(accessory->objectID);
 		string id("a_" + accessoryIdString);
 		div1.AddAttribute("id", id);
 		div1.AddClass("layout_item");
@@ -41,12 +24,12 @@ namespace webserver
 		div1.AddClass(state == AccessoryStateOn ? "accessory_on" : "accessory_off");
 		div1.AddAttribute("style", "left:" + to_string(layoutPosX) + "px;top:" + to_string(layoutPosY) + "px;");
 		div1.AddChildTag(HtmlTag("span").AddClass("symbola").AddContent("&#9209;"));
-		div1.AddChildTag(HtmlTag("span").AddClass("tooltip").AddContent(name + " (addr=" + to_string(address) + ")"));
+		div1.AddChildTag(HtmlTag("span").AddClass("tooltip").AddContent(accessory->name + " (addr=" + to_string(accessory->address) + ")"));
 
 		std::stringstream javascript;
 		javascript << "$(function() {"
-			" $('#" << id << "').on('click', function() { onClickAccessory(" << accessoryID << "); return false; });"
-			" $('#" << id << "').on('contextmenu', function(event) { if (event.shiftKey) return true; event.preventDefault(); onContextAccessory(" << accessoryID << "); return false; });"
+			" $('#" << id << "').on('click', function() { onClickAccessory(" << accessoryIdString << "); return false; });"
+			" $('#" << id << "').on('contextmenu', function(event) { if (event.shiftKey) return true; event.preventDefault(); onContextAccessory(" << accessoryIdString << "); return false; });"
 			"});"
 			;
 		div1.AddChildTag(HtmlTagJavascript(javascript.str()).AddClass("layout_item_script"));
@@ -57,7 +40,7 @@ namespace webserver
 		div2.AddAttribute("id", id + "_context");
 		div2.AddAttribute("style", "left:" + to_string(layoutPosX + 5) + "px;top:" + to_string(layoutPosY + 30) + "px;");
 		div2.AddChildTag(HtmlTag("ul").AddClass("contextentries")
-			.AddChildTag(HtmlTag("li").AddClass("contextentry").AddContent(name))
+			.AddChildTag(HtmlTag("li").AddClass("contextentry").AddContent(accessory->name))
 			.AddChildTag(HtmlTag("li").AddClass("contextentry").AddContent("Edit").AddAttribute("onClick", "loadPopup('/?cmd=accessoryedit&accessory=" + accessoryIdString + "');"))
 			.AddChildTag(HtmlTag("li").AddClass("contextentry").AddContent("Delete").AddAttribute("onClick", "loadPopup('/?cmd=accessoryaskdelete&accessory=" + accessoryIdString + "');"))
 			);
--- webserver/HtmlTagAccessory.h
+++ webserver/HtmlTagAccessory.h
@@ -12,20 +12,12 @@ namespace webserver
 	{
 		public:
 			HtmlTagAccessory(const datamodel::Accessory* accessory);
+
 			virtual HtmlTag AddAttribute(const std::string& name, const std::string& value) override
 			{
 				childTags[0].AddAttribute(name, value);
 				return *this;
 			}
-
-		private:
-			void Init(const accessoryID_t accessoryID,
-				const std::string& name,
-				const layoutPosition_t posX,
-				const layoutPosition_t posY,
-				const layoutPosition_t posZ,
-				const accessoryState_t state,
-				const address_t address);
 	};
 }; // namespace webserver
 
