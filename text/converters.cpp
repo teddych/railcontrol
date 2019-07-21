8c9791741bafa7b98c239a1a0c122ac1be971265
Teddy <teddy@teddy.ch>
2019-07-21 19:34:36 +0200

    Remove unused methods

--- text/converters.cpp
+++ text/converters.cpp
@@ -7,41 +7,9 @@ using std::string;
 
 namespace text
 {
-	void Converters::lockStatus(const lockState_t state, string& stateText) {
-		switch (state) {
-			case LockStateFree:
-				stateText.assign("free");
-				break;
-			case LockStateReserved:
-				stateText.assign("reserved");
-				break;
-			case LockStateSoftLocked:
-				stateText.assign("soft locked");
-				break;
-			case LockStateHardLocked:
-				stateText.assign("locked");
-				break;
-			default:
-				stateText.assign("unknown");
-		}
-	}
-
 	void Converters::accessoryStatus(const accessoryState_t state, string& onText)
 	{
 		onText.assign(state == datamodel::Accessory::AccessoryStateOn ? "green" : "red");
-	}
-
-	void Converters::feedbackStatus(const feedbackState_t state, string& stateText) {
-		switch (state) {
-			case FeedbackStateFree:
-				stateText.assign("free");
-				break;
-			case FeedbackStateOccupied:
-				stateText.assign("occupied");
-				break;
-			default:
-				stateText.assign("unknown");
-		}
 	}
 
 	void Converters::switchStatus(const switchState_t state, string& stateText)
