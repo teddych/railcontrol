6da294a3a8a49ac6c9342590fff7565589ece85b
Teddy <teddy@teddy.ch>
2019-10-24 17:57:54 +0200

    Fix minor bug in serialisation of layer

--- DataModel/Layer.h
+++ DataModel/Layer.h
@@ -36,6 +36,8 @@ namespace DataModel
 			Layer(const std::string& serialized) { Deserialize(serialized); }
 			Layer(__attribute__((unused)) Manager* manager, const layerID_t layerID) : Object(layerID) {}
 
+			virtual std::string Serialize() const { return "objectType=Layer;" + Object::Serialize(); }
+
 			objectType_t GetObjectType() const { return ObjectTypeLayer; }
 	};
 } // namespace DataModel
