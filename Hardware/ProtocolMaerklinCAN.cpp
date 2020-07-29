9e7a290cd53d391294262ebc19adeb595cc51bf0
Teddy <teddy@teddy.ch>
2020-07-29 21:51:19 +0200

    Fix SIGBUS problem on non x86/x64 hardware

--- Hardware/ProtocolMaerklinCAN.cpp
+++ Hardware/ProtocolMaerklinCAN.cpp
@@ -84,8 +84,14 @@ namespace Hardware
 		buffer[1] = (command << 1) | (response & 0x01);
 		Utils::Utils::ShortToDataBigEndian(hash, buffer + 2);
 		buffer[4] = length;
-		int64_t* data = (int64_t*) (buffer + 5);
-		*data = 0L;
+		buffer[5] = 0;
+		buffer[6] = 0;
+		buffer[7] = 0;
+		buffer[8] = 0;
+		buffer[9] = 0;
+		buffer[10] = 0;
+		buffer[11] = 0;
+		buffer[12] = 0;
 	}
 
 	void ProtocolMaerklinCAN::ParseAddressProtocol(const Address input, Address& address, Protocol& protocol)
