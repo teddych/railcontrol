38027c0bbc13b90cab468b9e2c5bc80ff1214d58
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2026-01-17 14:37:43 +0100

    Replace volatile stopSignalCounter ++ operator
    C++20 has deprecated the ++ and -- operations on volatile and C++26 is
    looking at removing them. This replaces them with an explicit update.

--- RailControl.cpp
+++ RailControl.cpp
@@ -47,7 +47,8 @@ using std::string;
 
 void killRailControlIfNeeded(Logger::Logger* logger)
 {
-	if (++stopSignalCounter < MaxStopSignalCounter)
+	stopSignalCounter = stopSignalCounter + 1;
+	if (stopSignalCounter < MaxStopSignalCounter)
 	{
 		return;
 	}
